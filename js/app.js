e0b71e85378beb5b9a57e5218115ea1923ff9b36
Keenan <42125735+Styleshift@users.noreply.github.com>
2023-05-02 16:28:53 -0400

    update js and css

--- js/app.js
+++ js/app.js
@@ -18,3 +18,29 @@ toggleBtn.addEventListener('click', () => {
         mobileMenu.style.display = 'none';
     }
 });
+
+/* document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    let query = window.matchMedia("(min-width: 600px)");
+
+    if(query.matches) {
+        //If the page is wider than 800px
+        //document.querySelector('.mobileLinks').style.display = "none";
+        document.querySelector('.mobileLinks').style.display = "none";
+    } else {
+        //If the page is narrower than 801px;
+        document.querySelector('.mobileLinks').style.display = "none";
+    }
+} */
+
+const matchResult = window.matchMedia("(min-width: 600px)");
+
+//console.log(matchResult)
+
+function isSmallScreen() {
+    if (window.matchMedia("(min-width: 600px)").matches) {
+        return true;
+}   return false;
+
+}
