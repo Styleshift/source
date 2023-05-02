3be1ebd23a0ff9912e1385383879bcc4e0353513
Keenan <42125735+Styleshift@users.noreply.github.com>
2023-05-02 14:07:27 -0400

    fix nav bar button

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const mobileMenu = document.querySelector('.mobileLinks');
 
 toggleBtn.addEventListener('click', () => {
     if(mobileMenu.style.display === 'none') {
-        mobileMenu.style.display = 'block';
+        mobileMenu.style.display = 'inline';
     } else {
         mobileMenu.style.display = 'none';
     }
